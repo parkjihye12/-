76670a73655b42343af4348cf1bfa7382d9fc51c
parkjihye12 <qkr-wlg@hanmail.net>
2024-04-30 15:37:04 +0900

    main

---
+++ main.js
@@ -0,0 +1,26 @@
+let result = 0;
+let operator = "";
+
+const resultEl = document.querySelector(".result");
+
+resultEl.innerHTML = result;
+
+function onClickNumber(number) {
+  if (resultEl.innerHTML.length >= 13) return;
+
+  if (resultEl.innerHTML === "0") {
+    resultEl.innerHTML = number;
+  } else {
+    resultEl.innerHTML += number;
+  }
+}
+
+function onClickOperator(inputOperator) {
+  result = +resultEl.innerHTML;
+
+  resultEl.innerHTML = 0;
+
+  operator = inputOperator;
+
+  console.log(operator);
+}
